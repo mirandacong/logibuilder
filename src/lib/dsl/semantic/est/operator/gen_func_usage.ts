c9c8895a690302dcc792cc32edd5182c1a78d2a1
Zecheng Xie <xiezecheng@logiocean.com>
2022-03-24 15:04:08 +0800

    chore: add func usage

--- src/lib/dsl/semantic/est/operator/gen_func_usage.ts
+++ src/lib/dsl/semantic/est/operator/gen_func_usage.ts
@@ -1,8 +1,7 @@
-import {getPlatform, Platform} from '@logi/base/ts/common/platform'
-import {getWorkspace} from '@logi/base/ts/spreadjs/bazel'
+/* eslint-disable indent */
+import {getWorkspace} from '@logi-base/src/ts/node/bazel'
 import {ArgumentParser} from 'argparse'
 import {writeFileSync} from 'fs'
-import {join} from 'path'
 
 import {getUsage, UsageArg, UsageFunc} from './usage'
 
@@ -14,10 +13,7 @@ const TEST_OUTPUT = 'func_usage.remarkup'
 /**
  * Use for generate the golden file.
  */
-const GOLDEN_OUTPUT = join(
-    getWorkspace(),
-    'src/lib/dsl/semantic/est/operator/func_usage.golden',
-)
+const GOLDEN_OUTPUT = 'src/lib/dsl/semantic/est/operator/func_usage.md'
 
 /**
  * Generate remarkup format file that used in phabricator.
@@ -36,34 +32,33 @@ function main(): void {
         writeFileSync(GOLDEN_OUTPUT, content, 'utf-8')
 }
 
-if (getPlatform() !== Platform.NODEJS || require?.main === module)
-    main()
+main()
 
 function genRemarkupFile(): string {
     const usage = getUsage()
     return `
-= Logi.Builder函数使用说明
+# Logi.Builder函数使用说明
 
-== 目录
+## 目录
 
-- [[#a | 行计算型函数]]
+- [行计算型函数](#行计算型函数)
 ${usage.rowType
     .map((s: UsageFunc, idx: number): string =>
-    `-- [[#a${idx} | ${s.name}]]`)
+            `  - [${s.name}](#a${idx})`)
     .join('\n')}
-- [[#b | 列计算型函数]]
+- [列计算型函数](#列计算型函数)
 ${usage.colType
     .map((s: UsageFunc, idx: number): string =>
-    `-- [[#b${idx} | ${s.name}]]`)
+            `  - [${s.name}](#b${idx})`)
     .join('\n')}
 
-== 行计算型函数 {anchor #a}
+## 行计算型函数
 
 ${usage.rowType
     .map((f: UsageFunc, i: number): string => genUsage(f, i, true))
     .join('\n\n')}
 
-== 列计算型函数 {anchor #b}
+## 列计算型函数
 
 ${usage.colType
     .map((f: UsageFunc, i: number): string => genUsage(f, i, false))
@@ -73,7 +68,7 @@ ${usage.colType
 
 function genUsage(s: UsageFunc, idx: number, isRowType: boolean): string {
     return `
-(NOTE) ${s.name}{anchor #${isRowType ? 'a' : 'b'}${idx}}
+> ${s.name} <span id='${isRowType ? 'a' : 'b'}${idx}'>
 
 - 函数描述
 
@@ -85,7 +80,7 @@ ${buildArgsInfo(s.args)}
 
 - 返回值类型
 
-    ##${s.returnType}##
+    **${s.returnType}**
 
 - 示例
 
@@ -96,19 +91,26 @@ ${buildArgsInfo(s.args)}
 function buildArgsInfo(args: readonly UsageArg[]): string {
     if (args.length === 0)
         return '    无参数'
-    return `<table>
-<tr><th>参数名</th><th>参数类型</th><th>可重复</th><th>可选</th><th>参数描述</th></tr>
+    return `  |参数名|参数类型|可重复|可选|参数描述|
+  |---|---|---|---|---|
 ${args
     .map((a: UsageArg): string => {
-        let str = '<tr>'
-        str += `<td>${a.name}</td>}`
-        str += `<td>##${a.types}##</td>`
-        str += `<td>${a.repeated ? '✔️' : '❌'}</td>`
-        str += `<td>${a.optional ? '✔️' : '❌'}</td>`
-        str += `<td>${a.description}</td>`
-        str += '</tr>'
-        return str
+        const items: string[] = [
+            a.name,
+            a.types,
+            a.repeated ? '✔️' : '❌',
+            a.optional ? '✔️' : '❌',
+            a.description,
+        ]
+        return `  |${items.join('|')}|`
+        // let str = ''
+        // str += `|${a.name}|`
+        // str += `|${a.types}|`
+        // str += `<td>${a.repeated ? '✔️' : '❌'}</td>`
+        // str += `<td>${a.optional ? '✔️' : '❌'}</td>`
+        // str += `<td>${a.description}</td>`
+        // str += '</tr>'
+        // return str
     })
-    .join('\n')}
-</table>`
+    .join('\n')}`
 }
